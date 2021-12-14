2d9f5159f8996f4b13e54290db595f2b09133b54
Jake <jake@Jakeui-iMac.local>
2021-12-15 00:32:11 +0900

    #8 Crating a Blick part Two

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index:number;
     public hash:string;
-    public previousHahs: string;
+    public previousHash: string;
     public data: string;
     public timestamp: number;
-    constructor(index:number,
-    hash:string,
-    previousHahs: string,
-    data: string,
-    timestamp: number) {
+
+    static calculateBlockhash = (index:number, previousHash:string, timestamp:number, data:string): any => {
+        CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    };
+
+    constructor(
+        index:number,
+        hash:string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ) {
         this.index = index;
         this.hash = hash;
-        this.previousHahs = previousHahs;
+        this.previousHash = previousHash;
         this.data = data;
         this.timestamp = timestamp
     }
@@ -19,7 +28,11 @@ class Block {
 
 const genesisBlock:Block = new Block(0, "202020202020202", "", "hello", 1235524);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain)
+const getBlockchain = () : Block[] => blockchain;
+
+const getLatestBlock = () : Block => getBlockchain[blockchain.length -1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime() / 1000)
 export{};
